c3003d4d72d7fc5fc2d8f627f54838e85e175b97
Nikolaj Bech Andersen <nikolaj.bech@live.dk>
2023-11-23 11:23:08 +0100

    Add docker DB and initial migration

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -49,8 +49,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   providers: [
     DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
